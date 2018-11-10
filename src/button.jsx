e35ad54f7438da10e049912c02019673025b690e
Carlosc23 <carlos@inegua.com>
2018-11-10 16:54:39 -0600

    fix: reduce code using map

--- src/button.jsx
+++ src/button.jsx
@@ -1,38 +1,34 @@
 import React, { Component } from 'react';
 import './button.css';
 import myObject from './utils/changeScreen';
+const buttons = ["C", "⁺∕₋", "%", "del", "7", "8", "9", "÷", "4", "5", "6", "x", "1", "2", "3", "-", "0", ".", "=", "+"];
 export default class button extends Component {
 
 
 render() {
-
   return(
 
-    <ul className="number-div">
-    <li id="equals3">C</li>
-    <li onClick={(e) => {myObject.updateText("-")}}>⁺∕₋</li>
-    <li>%</li>
-						<li>del</li>	             <li>7</li>
-										 <li>8</li>
-										 <li>9</li>
-<li id="equals2">÷</li>
-										 <li>4</li>
-										 <li>5</li>
-										 <li>6</li>
-<li id="equals2">x</li>
-										 <li>1</li>
-										 <li>2</li>
-										 <li>3</li>
-<li id="equals2">-</li>
-										 <li>0</li>
-										 <li>.</li>
+      <ul className="number-div">{
+         buttons.map((num) => {
+           if(num=="C"){
+             return  <li id="equals3" key={num} >{num}</li>
+           }
+           else if (num=="="){
+                return  <li id="equals" key={num} >{num}</li>
+           }
+           else if(num=="÷"||num=="x"||num=="-"||num=="+"){
+                 return  <li id="equals2" key={num} >{num}</li>
+           }
 
-										 <li id="equals">=</li>
- <li id="equals2">+</li>
-
-
-							   </ul>
+          else {
+              return  <li  key={num} >{num}</li>
+          }
+        }
+          )
+      }</ul>
   );
 }
+
+
 
 }
