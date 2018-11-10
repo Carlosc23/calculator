33867db6336b6b1a14bcfa09d3ded4f5b5737684
Carlosc23 <carlos@inegua.com>
2018-11-09 23:16:52 -0600

    feat: add screen component

--- src/calcBody.js
+++ src/calcBody.js
@@ -1,10 +1,13 @@
 import React, { Component } from 'react';
 import './calcBody.css'
+import Screen from './screen';
 export default class calcBody extends Component {
 render() {
   return(
 
-    <div className="main-div">Hellow </div>
+    <div className="main-div">
+<Screen/>
+    </div>
   );
 }
 
---
+++ src/screen.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+import './calcBody.css'
+export default class screen extends Component {
+render() {
+  return(
+
+    <div className="screen">Hello World </div>
+  );
+}
+
+}
