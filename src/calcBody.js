cac2cfa085076c2a223dbb093b6df0725de37c61
Carlosc23 <carlos@inegua.com>
2018-11-09 23:39:21 -0600

    feat: add buttons to body component

--- src/calcBody.js
+++ src/calcBody.js
@@ -1,12 +1,14 @@
 import React, { Component } from 'react';
 import './calcBody.css'
 import Screen from './screen';
+import Button from './button';
 export default class calcBody extends Component {
 render() {
   return(
 
     <div className="main-div">
 <Screen/>
+<Button/>
     </div>
   );
 }
