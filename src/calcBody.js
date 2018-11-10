51062758d4151a55d9985acf0eb093f370ff604b
Carlosc23 <carlos@inegua.com>
2018-11-09 23:06:02 -0600

    feat: add initial body of calculator

---
+++ src/calcBody.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+import './calcBody.css'
+export default class calcBody extends Component {
+render() {
+  return(
+
+    <div className="main-div">Hellow </div>
+  );
+}
+
+}
