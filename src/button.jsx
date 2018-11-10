843758653ba41e3f164acc5cb41a5eef94eb8058
Carlosc23 <carlos@inegua.com>
2018-11-10 00:30:12 -0600

    feat: more size to body of calculator

--- src/button.jsx
+++ src/button.jsx
@@ -5,22 +5,26 @@ render() {
   return(
 
     <ul class="number-div">
-							             <li>1</li>
-										 <li>2</li>
-										 <li>3</li>
-									     <li>+</li>
+    <li id="equals3">C</li>
+    <li>⁺∕₋</li>
+    <li>%</li>
+						<li>del</li>	             <li>7</li>
+										 <li>8</li>
+										 <li>9</li>
+<li id="equals2">÷</li>
 										 <li>4</li>
 										 <li>5</li>
 										 <li>6</li>
-									     <li>x</li>
-										 <li>7</li>
-										 <li>8</li>
-										 <li>9</li>
-									     <li>-</li>
+<li id="equals2">x</li>
+										 <li>1</li>
+										 <li>2</li>
+										 <li>3</li>
+<li id="equals2">-</li>
 										 <li>0</li>
 										 <li>.</li>
+
 										 <li id="equals">=</li>
-									     <li>÷</li>
+ <li id="equals2">+</li>
 
 
 							   </ul>
