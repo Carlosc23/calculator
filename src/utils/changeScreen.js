ad95a5b55450082c4b4b0179d0c7a8acff6701aa
Carlosc23 <carlos@inegua.com>
2018-11-10 22:56:36 -0600

    refactor: change double quotes to simple quotes

--- src/utils/changeScreen.js
+++ src/utils/changeScreen.js
@@ -1,44 +1,44 @@
-var currentText ="";
+var currentText ='';
 var flag = false;
 var flag2 = false;
-var temp ="";
+var temp ='';
 function updateText(text){
   currentText = temp;
   let last =text;
-  if(last=="÷"||last=="x"||last=="-"||last=="+"||last=="%"){
-    console.log("flag");
+  if(last=='÷'||last=='x'||last=='-'||last=='+'||last=='%'){
+    console.log('flag');
     flag =true;
   }
   console.log(currentText);
   last =currentText.slice(-1);
  console.log(currentText);
-  console.log("last"+last);
-  if(last=="÷"||last=="x"||last=="-"||last=="+"||last=="%"){
-    console.log("flag2");
+  console.log('last'+last);
+  if(last=='÷'||last=='x'||last=='-'||last=='+'||last=='%'){
+    console.log('flag2');
     flag2 = true;
   }
   currentText += text;
   console.log(text);
-  if (text==""){
-    currentText ="";
+  if (text==''){
+    currentText ='';
   }
-  else if(text=="del"){
+  else if(text=='del'){
     currentText = currentText.substring(0, currentText.length - 4);
   }
-  else if(text=="="){
+  else if(text=='='){
 
     currentText = currentText.substring(0, currentText.length - 1);
     console.log(currentText);
-    currentText = currentText.replace("x","*");
-    currentText = currentText.replace("÷","/");
+    currentText = currentText.replace('x','*');
+    currentText = currentText.replace('÷','/');
     currentText=eval(currentText);
     if (currentText.toString().includes('-') || currentText>999999999){
-      currentText="Math Error";
+      currentText='Math Error';
     }
     currentText = currentText.toString();
   }
-  else if(currentText.includes("Error")){
-    currentText="";
+  else if(currentText.includes('Error')){
+    currentText='';
   }
   console.log(currentText);
   if(currentText.length>=9){
@@ -55,7 +55,7 @@ function updateText(text){
     flag2 = false;
   }
   this.setState({currentText})
-//  console.log("this")
+//  console.log('this')
 }
 
 export default { updateText }
