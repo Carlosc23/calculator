6fb8b454801f9265272067300b20e31d5f581e6e
Carlosc23 <carlos@inegua.com>
2018-11-10 21:01:43 -0600

    feat: add style for each button

--- src/button.jsx
+++ src/button.jsx
@@ -9,19 +9,25 @@ render() {
   return(
 
       <ul className="number-div">{
-         buttons.map((num) => {
+         buttons.map((num,key) => {
            if(num=="C"){
-             return  <li id="equals3" key={num} >{num}</li>
+             console.log(num);
+             return  <li value={num} onClick={(e) => {myObject.updateText("")}} id="equals3" key={num}  >{num}</li>
            }
            else if (num=="="){
-                return  <li id="equals" key={num} >{num}</li>
+             console.log(num);
+                return  <li onClick={(e) => {myObject.updateText(num)}} id="equals" key={num} >{num}</li>
            }
            else if(num=="÷"||num=="x"||num=="-"||num=="+"){
-                 return  <li id="equals2" key={num} >{num}</li>
+             console.log(num);
+                 return  <li onClick={(e) => {myObject.updateText(num)}} id="equals2" key={num} >{num}</li>
+           }
+           else if(num=="⁺∕₋"){
+             return  <li   key={num}  >{num}</li>
            }
 
           else {
-              return  <li  key={num} >{num}</li>
+              return  <li onClick={(e) => {myObject.updateText(num)}}  key={num}  >{num}</li>
           }
         }
           )
