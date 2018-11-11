9212ba2ec93837df8c6a9f24005961a6bb81837e
Carlosc23 <carlos@inegua.com>
2018-11-10 22:45:39 -0600

    refactor: linter changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,8 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 import CalcBody from './calcBody';
 import Header from './Header';
+
 class App extends Component {
   render() {
     return (
--- src/calcBody.jsx
+++ src/calcBody.jsx
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import './calcBody.css'
+import './calcBody.css';
 import Screen from './screen';
 import Button from './button';
 export default class calcBody extends Component {
